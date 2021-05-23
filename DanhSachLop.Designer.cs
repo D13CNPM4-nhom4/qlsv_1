3777e49c192853128b7c9ad65aca3990c7db0b8e
AnhDuc2k <khungkhip@gmail.com>
2021-05-23 20:17:10 +0700

    create form ClassList

---
+++ DanhSachLop.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace QLSV
+{
+    partial class DanhSachLop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbkhoa = new System.Windows.Forms.ComboBox();
+            this.cbblop = new System.Windows.Forms.ComboBox();
+            this.btncapnhat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quayLaiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbbkhoa
+            // 
+            this.cbbkhoa.FormattingEnabled = true;
+            this.cbbkhoa.Location = new System.Drawing.Point(145, 29);
+            this.cbbkhoa.Name = "cbbkhoa";
+            this.cbbkhoa.Size = new System.Drawing.Size(180, 21);
+            this.cbbkhoa.TabIndex = 0;
+            this.cbbkhoa.SelectedIndexChanged += new System.EventHandler(this.cbbkhoa_SelectedIndexChanged);
+            // 
+            // cbblop
+            // 
+            this.cbblop.FormattingEnabled = true;
+            this.cbblop.Location = new System.Drawing.Point(145, 71);
+            this.cbblop.Name = "cbblop";
+            this.cbblop.Size = new System.Drawing.Size(180, 21);
+            this.cbblop.TabIndex = 1;
+            // 
+            // btncapnhat
+            // 
+            this.btncapnhat.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btncapnhat.Location = new System.Drawing.Point(435, 69);
+            this.btncapnhat.Name = "btncapnhat";
+            this.btncapnhat.Size = new System.Drawing.Size(91, 38);
+            this.btncapnhat.TabIndex = 2;
+            this.btncapnhat.Text = "CẬP NHẬT";
+            this.btncapnhat.UseVisualStyleBackColor = false;
+            this.btncapnhat.Click += new System.EventHandler(this.btncapnhat_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbbkhoa);
+            this.groupBox1.Controls.Add(this.cbblop);
+            this.groupBox1.Location = new System.Drawing.Point(24, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 122);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CHỌN LỚP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LỚP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "KHOA:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(754, 273);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quayLaiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // quayLaiToolStripMenuItem
+            // 
+            this.quayLaiToolStripMenuItem.Name = "quayLaiToolStripMenuItem";
+            this.quayLaiToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.quayLaiToolStripMenuItem.Text = "Quay lại";
+            this.quayLaiToolStripMenuItem.Click += new System.EventHandler(this.quayLaiToolStripMenuItem_Click);
+            // 
+            // DanhSachLop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btncapnhat);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DanhSachLop";
+            this.Text = "DanhSachLop";
+            this.Load += new System.EventHandler(this.DanhSachLop_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbkhoa;
+        private System.Windows.Forms.ComboBox cbblop;
+        private System.Windows.Forms.Button btncapnhat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quayLaiToolStripMenuItem;
+    }
+}
